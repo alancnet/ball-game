5e033c65edb8459c8cdd08ec4c92f3a3562f128b
Alan Colon <github@alanc.net>
2016-04-09 01:08:39 -0600

    Initial

---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(15 * Time.deltaTime, 45 * Time.deltaTime, 35 * Time.deltaTime);
+	}
+
+    // When collided by player
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            other.SendMessage("OnPickUpTrigger");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Text scoreText;
+    public float speed;
+    private Rigidbody rb;
+    private int pickUps;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        UpdateScore();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        rb.AddForce(x * speed, 0, z * speed);
+        
+    }
+
+    void OnPickUpTrigger()
+    {
+        pickUps++;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + pickUps;
+        }
+    }
+    
+}
